3ba30d725a82c9291db58b70fd7bca4510042f5a
game110pm <game110pm@605-110-STU07>
2017-02-13 12:43:58 -0700

    BYORK

---
+++ 2-13-17/2-13-17/woof woof.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int bork;
+	cout << "how many borks?" << endl;
+	cin >> bork;
+	for (int i = 0; i < bork; i++) {
+		cout << "bork" << endl;
+	}
+
+}
