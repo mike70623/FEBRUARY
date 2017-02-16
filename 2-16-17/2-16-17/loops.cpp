f10bab4aa724e206f1eca1dbf91c85da97022080
game110pm <game110pm@605-110-STU07>
2017-02-16 12:46:16 -0700

    loops

---
+++ 2-16-17/2-16-17/loops.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+	for (int i = 0; i <= 34; i++)
+	cout << i << endl;
+
+	for (int o = 26; o >= 3; o--)
+		cout << o << endl;
+
+	for (int p = 3; p < 30; p + 3)
+		cout << p << endl;
+}
