2063268a467462ed87ea9533c2adc2541c3c7bfe
game110pm <game110pm@605-110-STU07>
2017-02-24 12:55:17 -0700

    loops

--- 2-13-17/2-13-17/woof woof.cpp
+++ 2-13-17/2-13-17/woof woof.cpp
@@ -5,7 +5,7 @@ int main() {
 	cout << "how many borks?" << endl;
 	cin >> bork;
 	for (int i = 0; i < bork; i++) {
-		cout << "THE FITNESS GRAND PACER TEST IS A MULTI STAGE AROBIC TEST THAT PROGRESSIVLY GETSHARDER AS TIME GOES ON." << endl;
+		cout << "THE FITNESS GRAND PACER TEST IS A MULTI STAGE AROBIC TEST THAT PROGRESSIVLY GETS HARDER AS TIME GOES ON." << endl;
 	}
 
 }
---
+++ 2-24-17/2-24-17/mountain.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+int main() {
+	for (double i = 10; i <= 100000000000; i=i*10)
+		cout << i << endl;
+}
